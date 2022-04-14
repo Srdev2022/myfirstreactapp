6f34a9cb29f80d869ccfe6c17365c1bd60518bda
Srdev2022 <99554929+Srdev2022@users.noreply.github.com>
2022-04-14 19:04:44 -0400

    created a counter

---
+++ src/components/Button.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Button() {
+    const [count, setCount] = React.useState(0);
+
+    return (
+        <div>
+            <div>current count is: {count}</div>
+            <br/>
+            <button
+             onClick={e => {
+                 console.log(e)
+                 setCount(count + 1)
+             }}
+            >Increase</button>
+        </div>
+    )
+}
